caf779cbc64db305e5ecdd1bba656963ab3bea93
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2021-11-16 16:04:40 -0300

    Jogo-Memória

--- Jogo-Memória/script.js
+++ Jogo-Memória/script.js
@@ -1,13 +1,18 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard(){
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this
 }
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 })
-
-
-ksaddnsakfasnfade
-
